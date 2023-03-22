d8b69adf921cb7170105b7006a91812c61a91b8e
Paul Le Mercier <paul.lemercier05@gmail.com>
2023-03-22 16:55:42 +0400

    server and app

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const http = require('http')
+const app = require('./app.js')
 
-// Importing the routers
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
 
-// Connecting to the database
-mongoose.connect('mongodb+srv://lmpaul:rK7GUv2ER1vzpVa6@piquantecluster0.ceb0vvx.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
-// Configuring the CORS policy + JSON usage
-app.use(express.json())
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
-// Configuring the app routing
-
-
-module.exports = app
+server.listen(port);
